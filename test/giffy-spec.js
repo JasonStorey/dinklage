2bc7af50c8c088d40b72909fd8007eca54b1be41
Jason Storey <jason.andrew.storey@gmail.com>
2014-06-10 03:34:31 +0100

    Added testing infrastructure for DOM dependent code

--- test/giffy-spec.js
+++ test/giffy-spec.js
@@ -1,25 +1,39 @@
-var expect = require('chai').expect,
-	Giffy = require('../src/giffy');
+var chai = require('chai'),
+	sinon = require('sinon'),
+	sinonChai = require("sinon-chai"),
+	Giffy = require('../src/giffy'),
+	expect = chai.expect;
+
+chai.use(sinonChai);
 
 describe('Giffy', function() {
 
-	var mockConfig;
+	var mockConfig,
+		sandbox,
+		consoleLogStub;
 
 	beforeEach(function() {
 		mockConfig = {
 			img: ''
 		};
+
+	    sandbox = sinon.sandbox.create();
+        consoleLogStub = sandbox.stub(window.console, "log");
+	});
+
+	afterEach(function() {
+    	sandbox.restore();
 	});
 
 	describe('constructor', function() {
-		it('should store img element when passed in config', function() {
+		it('should store img element when present in config', function() {
 			var giffy,
-				expectedImg = '<img>'; 
+				mockImg = document.createElement('img');
 			
-			mockConfig.img = expectedImg;			
+			mockConfig.img = mockImg;			
 			giffy = new Giffy(mockConfig);
 
-			expect(giffy.img).to.equal(expectedImg);
+			expect(giffy.img).to.equal(mockImg);
 		});
 	});
 });
